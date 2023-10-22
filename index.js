d1d96ed7949951279b562c0952e18cdcb603caf2
Zarin Tasmin Sejuti <zts-7234@hotmail.com>
2023-10-22 20:06:25 +0300

    edited cors in index file and update vercel.json file

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // // Middleware
-const corsConfig = {
-  origin: '',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
+const corsOptions ={
+  origin:'*', 
+  credentials:true,
+  optionSuccessStatus:200,
 }
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+
+app.use(cors(corsOptions))
 // app.use(cors())
 app.use(express.json());
 
